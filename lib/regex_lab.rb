c1ed1486c0176fe1f5445a7e66a849f667f6b24a
Eric Clark <eclark0013@gmail.com>
2019-06-13 15:00:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/\A+un/)!=nil && text.match(/ing\z/)!=nil
+  text.scan(/\A+un/) && text.scan(/ing\z/)
 
 end
 
