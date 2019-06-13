ec92655d2a9815ee25241ccda3a130b7bbad53f9
Eric Clark <eclark0013@gmail.com>
2019-06-13 15:17:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,12 +8,12 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)!=nil
+  text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z].+\W\z/)
+  text.match(/\A[A-Z].+\W\z/)!=nil
 end
 
 def valid_phone_number?(phone)
